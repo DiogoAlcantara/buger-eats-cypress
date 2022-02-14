f5488bc8ed7947db1379cb0e5c53928f5de1fa72
Diogo Alcantara <diogo.alcantara1997@gmail.com>
2022-02-14 12:52:07 -0300

    mudando título do teste

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,5 +1,5 @@
 describe('Home page', () =>{
-    it('App deve estar online', () =>{
+    it('Testando se o app está online', () =>{
         cy.visit('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     })
